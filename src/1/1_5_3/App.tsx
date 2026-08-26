088665e9495adc0fa89541c815fc9b05e7b4b9de
Milevskaia_Elena <elena_307fk@mail.ru>
2026-08-26 19:22:32 +0600

    1_5_3

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -2,12 +2,20 @@
 /*
   Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
 */
+import React from 'react'; 
+
+function Card({children}: {children: React.ReactNode}){
+  return(
+    <div className="card">
+      <div className="card-content">{children}</div>
+    </div>
+  );
+}
 
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
+    <Card>
           <h1>Photo</h1>
           <img
             className="avatar"
@@ -16,14 +24,11 @@ export default function Profile() {
             width={70}
             height={70}
           />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
+    </Card>
+    <Card>
           <h1>About</h1>
           <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
+    </Card>
     </div>
   );
 }
