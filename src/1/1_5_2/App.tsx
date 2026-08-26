32b0debae47e8be5566a8b0a0439102c85e4f4ef
Milevskaia_Elena <elena_307fk@mail.ru>
2026-08-26 16:45:32 +0600

    1_5_2

--- src/1/1_5_2/App.tsx
+++ src/1/1_5_2/App.tsx
@@ -13,10 +13,11 @@ export type Person = {
 };
 
 function Avatar({ person, size }: { person: Person; size: number }) {
+  const sizeType = size < 90 ? 's' : 'b';
   return (
     <img
       className="avatar"
-      src={getImageUrl(person, "b")}
+      src={getImageUrl(person, sizeType)}
       alt={person.name}
       width={size}
       height={size}
@@ -26,12 +27,21 @@ function Avatar({ person, size }: { person: Person; size: number }) {
 
 export default function Profile() {
   return (
+    <>
     <Avatar
       size={40}
       person={{
         name: "Gregorio Y. Zara",
         imageId: "GregorioYZara",
-      }}
+      }} />
+      
+     <Avatar
+        size={160}
+        person={{
+          name: "Gregorio Y. Zara",
+          imageId: "GregorioYZara"
+     }}
     />
+    </>
   );
 }
