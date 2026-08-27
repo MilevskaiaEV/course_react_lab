09f2e7593116f6fb0d8b07bc407e292be26b11a9
Milevskaia_Elena <elena_307fk@mail.ru>
2026-08-27 12:24:07 +0600

    1_6_3

--- src/1/1_6_3/App.tsx
+++ src/1/1_6_3/App.tsx
@@ -4,24 +4,30 @@
 */
 
 function Drink({ name }: { name: string }) {
+  let part: string;
+  let caffeine: string;
+  let age: string;
+
+ if (name === 'tea') {
+    part = 'leaf';
+    caffeine = '15–70 mg/cup';
+    age = '4,000+ years';
+  } else {
+    part = 'bean';
+    caffeine = '80–185 mg/cup';
+    age = '1,000+ years';
+  }
+
   return (
       <section>
           <h1>{name}</h1>
           <dl>
               <dt>Part of plant</dt>
-              <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
+              <dd>{part}</dd>
               <dt>Caffeine content</dt>
-              <dd>
-                  {name === 'tea'
-                      ? '15–70 mg/cup'
-                      : '80–185 mg/cup'}
-              </dd>
+              <dd>{caffeine}</dd>
               <dt>Age</dt>
-              <dd>
-                  {name === 'tea'
-                      ? '4,000+ years'
-                      : '1,000+ years'}
-              </dd>
+              <dd>{age}</dd>
           </dl>
       </section>
   );
